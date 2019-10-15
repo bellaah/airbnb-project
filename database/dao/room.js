970ad9728f97c1cdc431f3a81b1f6803deb5cd8c
heeesun <gmltjs1996@naver.com>
2019-10-15 21:56:10 +0900

    feat: make create function in dao/Room

--- database/dao/room.js
+++ database/dao/room.js
@@ -1,7 +1,18 @@
 const { room } = require("../index");
 
 class Room {
-
+    create(obj) {
+        return room.create({
+            title : obj.title,
+            hostID : obj.hostID,
+            star : obj.star,
+            img : obj.img,
+            price : obj.price,
+            review : obj.review,
+            capacity : obj.capacity,
+        });
+    }
+    
 }
 
 module.exports = Room;
